5c5060ba4af727a37f70d3f8d0b89bd04c03e4fb
DertyuDev <sidovidchenko@gmail.com>
2025-03-03 19:22:51 +0300

    2

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def tr(prof):
     return render_template('base.html', prof=prof)
 
+@app.route('/list_prof/<list1>')
+def li(list1):
+    items = [str(i) * 10 for i in range(10)]
+    return render_template('list.html', list=list1, items=items)
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
