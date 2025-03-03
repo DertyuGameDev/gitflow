4f32930b7c202394e050259f4d33f91930753a60
DertyuDev <sidovidchenko@gmail.com>
2025-03-03 20:20:02 +0300

    4

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from flask import Flask, render_template, redirect
 from loginform import LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/')
+def index(prof):
+    return render_template('base.html', prof=prof)
 
 
 @app.route('/training/<prof>')
@@ -23,6 +29,14 @@ def login():
     for i in 'title surname name education profession sex motivation ready'.split():
         param[i] = 1
     return render_template('auto_answer.html', **param)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login1():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
